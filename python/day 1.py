af79bc3fd52f57e043de7b03ec7ef61bfd1349cd
Prasannnnn <prasannamb4362@gmail.com>
2024-06-17 21:02:31 +0530

    Data Analysis with python

---
+++ python/day 1.py
@@ -0,0 +1,27 @@
+## tamil song
+
+
+# print("""Kurumugilgalai sirumugaigalil 
+#       Yaar thoovinaar 
+#       Mazhaikondu kavidhai theetinaar 
+#       Ilam piraiyinai idhazh idaiyinil 
+#       Yaar sootinaar 
+#       Sirithidum silaiyai kaattinaar 
+#       Erumbugal sumandhu pogudhae""")
+
+## English song
+# print("""Kiss me hard before you go
+# Summertime sadness
+# I just wanted you to know
+# That, baby, you the best
+# I got my red dress on tonight
+# Dancin' in the dark, in the pale moonlight
+# See more""")
+
+
+###########################################################################################
+
+#Variables
+b = 55
+a = "five          five"
+print("Krishnan weight is ",b," kg")
