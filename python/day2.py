4a74d8e659f4aa86ad3566eed7fb409ea200b56e
Prasannnnn <prasannamb4362@gmail.com>
2024-06-18 20:12:06 +0530

    Tasks 1

--- python/day2.py
+++ python/day2.py
@@ -1,13 +1,26 @@
 '''
 User Input
 '''
-a = int(input("Enter a Value: "))
-b = int(input("Enter b Value: "))
-c = a + b
-print(c)
+# a = int(input("Enter a Value: "))
+# b = int(input("Enter b Value: "))
+# c = a + b
+# print(c)
 
 # a = 4
 # b = 8
 # c = a+b
 # print(c)
 # print(type(c))
+
+'''
+Tasks 1
+a = 65.25
+b = 00.25
+c = a + b
+c = 65.50
+'''
+
+a = float(input("Enter a Value: "))
+b = float(input("Enter b Value: "))
+c = a + b
+print(c)
