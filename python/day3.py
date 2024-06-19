84dad8752d2c852581c4a2120fb2b274a95db66e
Prasannnnn <prasannamb4362@gmail.com>
2024-06-19 20:37:27 +0530

    day3.py

---
+++ python/day3.py
@@ -0,0 +1,37 @@
+'''
+Types of Operators
+--->Arithmetic Operator
+--->Comparison Operator
+--->Assignment Operator
+--->Logical Operator
+--->Boolean Operator
+--->Membership Operators
+--->Identify Operator
+--->Bitwise Operator
+
+
+
+
+Arithmetic Operators:
+---> Addition +
+---> Sub -
+---> Multiplication *
+---> Division /
+---> Modulus %
+---> Floor Division //
+---> Exponentiol **
+
+Assignment Operator:
+---> a = b+c
+---> a += 10
+---> a -= 10
+---> a *= 10
+---> a /= 10
+---> a //= 10
+---> a %= 10
+---> a**= 10
+
+'''
+a = 5
+a += 10 # a = a + 10 ----> a = 5 + 10 ---> a = 15
+print(a) 
