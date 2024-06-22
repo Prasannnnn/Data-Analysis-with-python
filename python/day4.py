14027dcc1c5663dc3eaab17bf19ef5c0cdfd4a9c
Prasannnnn <prasannamb4362@gmail.com>
2024-06-22 19:52:07 +0530

    changes of day4.py

--- python/day4.py
+++ python/day4.py
@@ -1,3 +1,13 @@
 a = float(input("Enter the value: "))#-->9
 if a == 5: # ---> 9 == 5
     print("it is an positive")
+
+
+#########
+a = float(input())
+b = float(input())
+if a > b:
+    print(f"{a} greater than {b}")
+else:
+    print(f"{b} is greather than {a}")
+
