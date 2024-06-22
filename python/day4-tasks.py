62de50ad7315c42688e1ccd389ab058473d2e40e
Prasannnnn <prasannamb4362@gmail.com>
2024-06-22 20:47:20 +0530

    day4.py

--- python/day4-tasks.py
+++ python/day4-tasks.py
@@ -5,5 +5,9 @@ if c == "+":
     print(f"the addition value of {a}and {b} is {a+b}")
 elif c == "*":
     print(f"the multiplication value of {a} and {b} is {a*b}")
+elif c == "-":
+    print(f"the subtraction value of {a} and {b} is {a-b}")
+elif c == "/":
+    print(f"the multiplication value of {a} and {b} is {a/b}")
 else:
     print(f"{c} is an invalid Operator")
