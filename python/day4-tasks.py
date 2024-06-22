5c5857c327d21661477e2cf44bbec922be10b2eb
Prasannnnn <prasannamb4362@gmail.com>
2024-06-22 20:48:13 +0530

    day4.py

--- python/day4-tasks.py
+++ python/day4-tasks.py
@@ -8,6 +8,6 @@ elif c == "*":
 elif c == "-":
     print(f"the subtraction value of {a} and {b} is {a-b}")
 elif c == "/":
-    print(f"the multiplication value of {a} and {b} is {a//b}")
+    print(f"the multiplication value of {a} and {b} is {a/b}")
 else:
     print(f"{c} is an invalid Operator")
