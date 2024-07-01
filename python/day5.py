66f9ade9a21361cc86ad657453506d3c4002bd6d
Prasannnnn <prasannamb4362@gmail.com>
2024-07-01 20:07:21 +0530

    day5.py

---
+++ python/day5.py
@@ -0,0 +1,12 @@
+'''
+area of circle = pi*r**2
+'''
+print("Enter the operator: circle")
+c = input("Enter the Angle: ")
+if c == "circle":
+    pi = 22/7
+    r  = float(input("Enter the radius: "))
+    print(pi*r**2)
+
+else:
+    print("Invalid Angle")
