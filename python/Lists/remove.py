c0ec2ae6e8bcb6cc392479e796bcf5fa03cb9085
Prasannnnn <prasannamb4362@gmail.com>
2024-07-02 20:26:53 +0530

    remove.py

--- python/Lists/remove.py
+++ python/Lists/remove.py
@@ -1,8 +1,15 @@
 a = ["Pulsar","Rajoot","Royal Enfield","RX 100","MT","Ronin","Apache"]
-a.remove("Ronin")
-a.pop(3)
-#del a
-print(a)
+# a.remove("Ronin")
+# a.pop(3)
+# #del a
+# print(a)
+# b = ["1","Raj","Royal ","RX ","MT","Roni","Apa"]
+# b.remove("Raj")
+# print(b)
 b = ["1","Raj","Royal ","RX ","MT","Roni","Apa"]
-b.remove("Raj")
-print(b)
+a.extend(b)
+print(a)
+print(len(a))
+a.remove("Raj")
+a.pop(5)
+print(a)
