049c72c46d758b05616214255e1f14949ee04084
Prasannnnn <prasannamb4362@gmail.com>
2024-07-02 19:51:29 +0530

    change.py

---
+++ python/Lists/change.py
@@ -0,0 +1,7 @@
+a = ["Kingfisher","Jinga Gold","Vodka","Diamond","Red Bull"]
+#--->print(a)
+# list is an ordered
+# is there possible to change lists
+#--->a[4] = "Old Monk"
+a[1:3]=["Magic Moments","barcadi"]
+print(a)
