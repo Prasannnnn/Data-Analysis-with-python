699652f88f126cbfa3e3771b11961e5292b8f85a
Prasannnnn <prasannamb4362@gmail.com>
2024-07-03 20:28:15 +0530

    remove.py

--- python/Lists/loop_list.py
+++ python/Lists/loop_list.py
@@ -1,4 +1,15 @@
 #for loop ---> lists, tuple,set and dictionary
 a = ["Strawberry","Guava","Avocoda","Litchy","Mango"]
-for x in a:
-    print(x)
+#for x in range(len(a)):
+#    print(x)
+
+'''
+0
+1
+2
+3
+4
+'''
+
+for y in range(5000):
+    print(y)
