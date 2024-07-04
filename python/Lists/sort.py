a81ef29458115cae4d0c99efd8b54d53825185ed
Prasannnnn <prasannamb4362@gmail.com>
2024-07-04 19:58:04 +0530

    sort.py

--- python/Lists/sort.py
+++ python/Lists/sort.py
@@ -1,7 +1,12 @@
+#sort lists
 # a = ["Gopala Krishnan","Prasanna Balaji","Hemakkani","Priya","Ashwath","Bhuvana","Pranav","Pranash","Prasath","Rajesh"]
 # a.sort()
 # print(a)
 
-a = ['apple','Mango',"Kiwi","Guavua","Green apple","bread Omelette"]
-a.sort()
+# a = ['apple','Mango',"Kiwi","Guavua","Green apple","bread Omelette"]
+# a.sort()
+# print(a)
+
+a = ["Gopala Krishnan","Prasanna Balaji","Hemakkani","Priya","Ashwath"]
+a.sort(reverse=True)
 print(a)
