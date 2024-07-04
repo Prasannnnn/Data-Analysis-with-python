d92d750010d325855ba1bdcd5172dc29f722eb91
Prasannnnn <prasannamb4362@gmail.com>
2024-07-04 20:16:03 +0530

    copy.py

--- python/Lists/copy.py
+++ python/Lists/copy.py
@@ -1,3 +1,10 @@
-a = ["Gopala Krishnan","Vijay","SK","Vijaysethupathi","Vijaya devarkonda","Allu Arjun","DQ","SRK"]
-b = a.copy()
+#copy the lists
+# a = ["Gopala Krishnan","Vijay","SK","Vijaysethupathi","Vijaya devarkonda","Allu Arjun","DQ","SRK"]
+# b = a.copy()
+# print(b)
+
+
+# use the lists method
+a = ['apple','Mango',"Kiwi","Guavua","Green apple","bread Omelette"]
+b = list(a)
 print(b)
