6c3482323f825b86a72360b5912392d4345e3c98
Prasannnnn <prasannamb4362@gmail.com>
2024-07-04 19:54:57 +0530

    sort.py

--- python/Lists/sort.py
+++ python/Lists/sort.py
@@ -1,3 +1,7 @@
-a = ["Gopala Krishnan","Prasanna Balaji","Hemakkani","Priya","Ashwath","Bhuvana","Pranav","Pranash","Prasath","Rajesh"]
+# a = ["Gopala Krishnan","Prasanna Balaji","Hemakkani","Priya","Ashwath","Bhuvana","Pranav","Pranash","Prasath","Rajesh"]
+# a.sort()
+# print(a)
+
+a = ['apple','Mango',"Kiwi","Guavua","Green apple","bread Omelette"]
 a.sort()
 print(a)
