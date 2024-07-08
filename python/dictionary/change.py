e037f551270c6b18c6abed6cab53930cb08c9c88
Prasannnnn <prasannamb4362@gmail.com>
2024-07-08 19:54:22 +0530

    change.py

---
+++ python/dictionary/change.py
@@ -0,0 +1,10 @@
+a = {
+    "name" : "Prasanna",
+    "age" : 22,
+    "College" : "Sathyabama",
+    "State":"Pondicherry"
+}
+a ["name"] = "Krishnan"
+a["State"] = "Tamilnadu"
+a.update({"year":"2001"})
+print(a)
