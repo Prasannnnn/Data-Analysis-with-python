4a513894c069bf6eb39fed6b9eab0285b3d3095c
Prasannnnn <prasannamb4362@gmail.com>
2024-07-08 19:49:49 +0530

    access.py

---
+++ python/dictionary/access.py
@@ -0,0 +1,18 @@
+a = {
+    "name" : "Prasanna",
+    "age" : 22,
+    "College" : "Sathyabama",
+    "State":"Pondicherry"
+}
+x = a.keys()
+
+a ["Height"] = "6ft"
+
+
+print(x)
+
+y = a.values()
+
+print(y)
+
+print(a.get("name"))
