e7f4aeeca3dad6fdad5a4734a7cecfcd765f5d53
Prasannnnn <prasannamb4362@gmail.com>
2024-07-09 16:58:40 +0530

    loop.py

---
+++ python/dictionary/loop.py
@@ -0,0 +1,11 @@
+a={
+    "name" : "Prasanna",
+    "age" : 22,
+    "College" : "Sathyabama",
+    "State":"Pondicherry"
+}
+for x in a:
+    print("the for loop",x)
+
+for y in a:
+    print(a[y])
