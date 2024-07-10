bbf996bd9c63e996543232c46eb16ac16fc6b172
Prasannnnn <prasannamb4362@gmail.com>
2024-07-10 20:32:52 +0530

    odd_even

---
+++ python/problems/odd _even.py
@@ -0,0 +1,16 @@
+# num = float(input("Enter the number: "))
+
+# if num % 2 == 0:
+#     print(f"{num} is a even number")
+# else:
+#     print(f"{num} is a odd number")
+
+
+num = float(input("Enter the number: "))
+
+if num % 2 == 0 and num >= 6 and num <= 20:
+    print("Not Weird")
+elif num % 2 == 0 and num >= 20:
+    print("weird Greater")
+else:
+    print("Weird")
