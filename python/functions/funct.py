03f533e8614ad8c5f1f8ff49b9c4162697b1fb23
Prasannnnn <prasannamb4362@gmail.com>
2024-07-17 19:27:33 +0530

    funct.py

--- python/functions/funct.py
+++ python/functions/funct.py
@@ -1,4 +1,28 @@
 def add(a,b):
-    print(a+b)
-add(10,5)
+    return a+b
 
+def sub(a,b):
+    return a-b
+
+def mult(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+
+def floor(a,b):
+    return a//b
+
+
+def mod(a,b):
+    return a%b
+
+
+a = float(input("Enter the a Value: "))
+b = float(input("Enter the b Value: "))
+print("Enter the operator of add, sub, mult, div, floor and mod")
+operator = input("Enter the operator")
+
+if operator == "add":
+    print(f"the addition of {a} and {b} is {add(a,b)}")
